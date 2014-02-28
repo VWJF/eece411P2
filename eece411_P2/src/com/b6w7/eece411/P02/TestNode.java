0949c31568fb16b1f5efadde21513cbcf95bd8e3
Scott <scotthazlett@gmail.com>
2014-02-27 22:25:19 -0800

    Removed unused imports

--- eece411_P2/src/com/b6w7/eece411/P02/TestNode.java
+++ eece411_P2/src/com/b6w7/eece411/P02/TestNode.java
@@ -1,14 +1,11 @@
 package com.b6w7.eece411.P02;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketImpl;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.BufferOverflowException;
