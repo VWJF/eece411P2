8a16b039448ae48ad1aca35a5d29232d83d3323b
vfjf <i.sahay@alumni.ubc.ca>
2014-02-28 05:08:51 -0800

    Untested. -Completed Launcher.java. Currently clients request are serially serviced , but the implementation is such that it is multi-threadable (must verify concurrency will be maintained). -Created ClientInterface which acts a structure to store client socket and the elements of its Requests and Replies. ClientInterface uses a Map to store (key,value) pairs.

--- eece411_P2/src/com/b6w7/eece411/P02/Launcher.java
+++ eece411_P2/src/com/b6w7/eece411/P02/Launcher.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -21,53 +23,93 @@ public class Launcher {
 	private static final int NUM_CLIENTS = 15;
 	
 	//private static ArrayList<Socket> connected_clients;
-	private static ArrayBlockingQueue<Element<Socket,TestData>> connected_clients;
+	private static ArrayBlockingQueue<ClientInterface> connected_clients;
 	
 	public static void main(String[] args) throws IOException {
 		
 		//connected_clients = new ArrayList<Socket>(NUM_CLIENTS);
-		connected_clients = new ArrayBlockingQueue<Element<Socket,TestData>>(NUM_CLIENTS);
+		connected_clients = new ArrayBlockingQueue<ClientInterface>(NUM_CLIENTS);
 		
 		int servPort = Integer.parseInt(args[0]);
 
 		// Create a server socket to accept client connection requests
 		ServerSocket servSock = new ServerSocket(servPort);
 
-		int recvMsgSize;   // Size of received message
-		byte[] byteBuffer = new byte[REQSIZE];  // Receive buffer
-		char[] charBufferIn = new char[REQSIZE];  // Receive buffer
+		int recvMsgSize = 0;   // Size of received message
+		//byte[] byteBuffer = new byte[REQSIZE];  // Receive buffer
+		char[] charBufferIn = new char[REQSIZE];  // Receive buffer for BufferedReader inFromClient
 		byte[] byteBufferOut = new byte[RESSIZE];  // Response buffer
 
 		for (;;) { // Run forever, accepting and servicing connections
-			Socket clntSock = servSock.accept();     // Get client connection
-			
-	
-			
+		
+		// Thread 1 accepting connections			
+
+			Socket clientSock = servSock.accept();     // Get client connection, Blocking call
+		
 			System.out.println("Handling client at " +
-					clntSock.getInetAddress().getHostAddress());
+					clientSock.getInetAddress().getHostAddress());
 
-			InputStream in = clntSock.getInputStream();
-			OutputStream out = clntSock.getOutputStream();
-
+			//InputStream in = clientSock.getInputStream();
+			//OutputStream out = clientSock.getOutputStream();
 			DataOutputStream outToClient = 
-					new DataOutputStream(clntSock.getOutputStream());
+					new DataOutputStream(clientSock.getOutputStream());
 			BufferedReader inFromClient = 
-					new BufferedReader(new InputStreamReader(clntSock.getInputStream()), REQSIZE );
-			
-			
-			// Receive until client closes connection, indicated by -1 return
-			while ((recvMsgSize = inFromClient.read(charBufferIn, 0, REQSIZE)) != -1)
-				outToClient.write(byteBufferOut, 0, recvMsgSize);
-			
-			TestData sample1 = null;
+					new BufferedReader(new InputStreamReader(clientSock.getInputStream()), REQSIZE );
+				
 			try {
-				connected_clients.add(new Element<Socket,TestData>(clntSock,sample1) );
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-				System.out.println("Max connected clients.\n"+e.getLocalizedMessage());
+				// Receive REQSIZE number of bytes or 
+				// until client closes connection, indicated by -1 return 
+				while ((recvMsgSize = inFromClient.read(charBufferIn, 0, REQSIZE)) != -1)
+					;
+					
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}			
+			
+			//Parse and Extract relevant data
+			ByteBuffer dataRead = ByteBuffer.wrap(	new String(charBufferIn).getBytes() );
+			byte cmd = dataRead.get();		//Read 1 byte
+			byte[] key = new byte[32];		dataRead.get(key, 1, key.length);
+			byte[] value = new byte[1024];	dataRead.get(value, 33, value.length);
+			
+			ClientInterface ci = new ClientInterface(clientSock, cmd, 
+												ByteBuffer.wrap(key), 
+												ByteBuffer.wrap(value) );
+			
+			// Add client socket and received command to the queue of connected clients.
+			if( !connected_clients.offer(ci) ){
+				System.out.println("Max connected clients.\n");
+				clientSock.close();
+				continue;
 			}
 			
-			clntSock.close();  // Close the socket.  We are done with this client!
+		// Thread 2 executing commands			
+			//Execute received command.
+			
+			// TODO: Currently ClientInterface.getReply() will execute the command before generating a reply.
+			// getReply() is called from the Thread sending replies.
+
+			
+		// Thread 3 Sending replies commands			
+			
+			//Obtain a connected client and reply to the client with its response.
+			ClientInterface clientToReply =	connected_clients.poll();
+
+			// Send reply to client
+			if(clientToReply != null){
+				try {
+					byteBufferOut= clientToReply.getReply().array();
+					outToClient.write(byteBufferOut, 0, recvMsgSize);
+
+				} catch (IOException e1) {
+					// TODO:
+					e1.printStackTrace();
+				}
+				clientToReply.getSocket().close(); // Close the socket.  We are done with this client!
+			}
+			
+			
+			//clientSock.close();  // Close the socket.  We are done with this client!
 		}
 	}
 }
--- eece411_P2/src/com/b6w7/eece411/P02/NodeCommands.java
+++ eece411_P2/src/com/b6w7/eece411/P02/NodeCommands.java
@@ -1,5 +1,11 @@
 package com.b6w7.eece411.P02;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.b6w7.eece411.P02.NodeCommands.Reply;
+
 public class NodeCommands {
 	final static byte CMD_PUT = 0x1;
 	final static byte CMD_GET = 0x2;
@@ -9,6 +15,72 @@ public class NodeCommands {
 	final static byte RPY_INEXISTENT = 0x1;
 	final static byte RPY_OUT_OF_SPACE = 0x2;
 	final static byte RPY_OVERLOAD = 0x3;
-	final static byte RPY_byteERNAL_FAILURE = 0x4;
+	final static byte RPY_INTERNAL_FAILURE = 0x4;
 	final static byte CMD_UNRECOGNIZED = 0x5;
+
+	// Need to make a check in ClientInterface whether the "received" command/error code 
+	// were among eligible commands.
+	// Separated into 2 Enums with Request Commands and Reply Commands
+	static public enum Request{
+		CMD_PUT(1), 
+		CMD_GET(2), 
+		CMD_REMOVE(3);
+		
+		private int value;
+
+		private Request(int value) {
+			this.value = value;
+		}
+		public int getCode() {
+			return value;
+		}
+		// Source:
+		// http://howtodoinjava.com/2012/12/07/guide-for-understanding-enum-in-java/
+		// Lookup table
+		private static final Map<Integer, Request> lookup = new HashMap<Integer, Request>();
+
+		// Populate the lookup table on loading time
+		static {
+			for (Request s : EnumSet.allOf(Request.class))
+				lookup.put(s.getCode(), s);
+		}
+
+		// This method can be used for reverse lookup purpose
+		public static Request get(int code) {
+			return (Request) lookup.get(code);
+		}
+	};
+		
+	static public enum Reply{
+		RPY_SUCCESS(0),
+		RPY_INEXISTENT(1),
+		RPY_OUT_OF_SPACE(2),
+		RPY_OVERLOAD(3),
+		RPY_INTERNAL_FAILURE(4),
+		CMD_UNRECOGNIZED(5);
+
+		private int value;
+
+		private Reply(int value) {
+			this.value = value;
+		}
+		public int getCode() {
+			return value;
+		}
+		// Source:
+		// http://howtodoinjava.com/2012/12/07/guide-for-understanding-enum-in-java/
+		// Lookup table
+		private static final Map<Integer, Reply> lookup = new HashMap<Integer, Reply>();
+
+		// Populate the lookup table on loading time
+		static {
+			for (Reply s : EnumSet.allOf(Reply.class))
+				lookup.put(s.getCode(), s);
+		}
+
+		// This method can be used for reverse lookup purpose
+		public static Reply get(int code) {
+			return (Reply) lookup.get(code);
+		}
+	};   
 }
