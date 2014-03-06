fafd956971ed0f255880bfdec2eba110ba2149d6
Scott <scotthazlett@gmail.com>
2014-03-05 18:20:24 -0800

    minor fixes

--- eece411_P2/src/com/b6w7/eece411/P02/Test/TestNode.java
+++ eece411_P2/src/com/b6w7/eece411/P02/Test/TestNode.java
@@ -119,30 +119,29 @@ public class TestNode {
 	private static void populateTests() throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		// test 1: put 'Scott' => '63215065', and so on ... 
 
-		//		populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
-		//		populateOneTest(NodeCommands.CMD_REMOVE, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
-		/*
-		populateOneTest(NodeCommands.CMD_PUT, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_PUT, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_PUT, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
+				populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
+//				populateOneTest(NodeCommands.CMD_REMOVE, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
+//
+//		populateOneTest(NodeCommands.CMD_PUT, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_PUT, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_PUT, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
+//
+//		populateOneTest(NodeCommands.CMD_PUT, "John", "Smith", NodeCommands.RPY_OUT_OF_SPACE);
+//
+//		populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_GET, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_GET, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
+//
+//		populateOneTest(NodeCommands.CMD_REMOVE, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_REMOVE, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
+//		populateOneTest(NodeCommands.CMD_REMOVE, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
+//
+//		populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
+//		populateOneTest(NodeCommands.CMD_GET, "Ishan", "Sahay", NodeCommands.RPY_INEXISTENT);
+//
+//		populateOneTest(NodeCommands.CMD_GET, "localhost", "137.82.52.29", NodeCommands.RPY_INEXISTENT);
 
-		populateOneTest(NodeCommands.CMD_PUT, "John", "Smith", NodeCommands.RPY_OUT_OF_SPACE);
-
-		populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_GET, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_GET, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
-
-		populateOneTest(NodeCommands.CMD_REMOVE, "Scott", "63215065", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_REMOVE, "Ishan", "Sahay", NodeCommands.RPY_SUCCESS);
-		populateOneTest(NodeCommands.CMD_REMOVE, "ssh-linux.ece.ubc.ca", "137.82.52.29", NodeCommands.RPY_SUCCESS);
-
-		populateOneTest(NodeCommands.CMD_GET, "Scott", "63215065", NodeCommands.RPY_INEXISTENT);
-		populateOneTest(NodeCommands.CMD_GET, "Ishan", "Sahay", NodeCommands.RPY_INEXISTENT);
-
-		populateOneTest(NodeCommands.CMD_GET, "localhost", "137.82.52.29", NodeCommands.RPY_INEXISTENT);
-		 */
-
-		populateOneTest(NodeCommands.CMD_UNRECOGNIZED, "Fake", "Fake", NodeCommands.RPY_UNRECOGNIZED_CMD);
+//		populateOneTest(NodeCommands.CMD_UNRECOGNIZED, "Fake", "Fake", NodeCommands.RPY_UNRECOGNIZED_CMD);
 
 	}
 
@@ -186,9 +185,8 @@ public class TestNode {
 
 			// we will use this stream to send data to the server
 			// we will use this stream to receive data from the server
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			//InputStream inFromServer = clientSocket.getInputStream();
-			BufferedInputStream inFromServer = new BufferedInputStream(clientSocket.getInputStream() );
+			DataOutputStream outToServer = null;
+			BufferedInputStream inFromServer = null;
 
 			// Loop through all tests
 			byte[] recvBuffer = new byte[NodeCommands.LEN_VALUE_BYTES];
@@ -212,6 +210,10 @@ public class TestNode {
 					clientSocket = new Socket("localhost", serverPort);
 					clientSocket.setSoTimeout(TCP_READ_TIMEOUT_MS);
 					System.out.println("Connected to server ...");
+
+					outToServer = new DataOutputStream(clientSocket.getOutputStream());
+					inFromServer = new BufferedInputStream(clientSocket.getInputStream() );
+
 					outToServer.write(test.buffer.array());
 					//StandardCharsets.UTF_8.displayName()
 
@@ -302,7 +304,7 @@ public class TestNode {
 					testFailed++;
 
 				} finally {
-					if (clientSocket != null) { 
+					if (clientSocket != null && null != inFromServer) { 
 						try {
 							while (inFromServer.read(recvBuffer, 0, recvBuffer.length) > 0) {
 								// regardless of whether the test passed or failed,
--- eece411_P2/src/com/b6w7/eece411/P02/multithreaded/HandlerThread.java
+++ eece411_P2/src/com/b6w7/eece411/P02/multithreaded/HandlerThread.java
@@ -4,28 +4,30 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class HandlerThread extends Thread implements PostCommand {
 	private final ConcurrentLinkedQueue<Command> inQueue = new ConcurrentLinkedQueue<Command>();
-	
+
 	// TODO make private with accessor
 	public boolean keepRunning = true;
-	
+
 	public HandlerThread() {
-		
+
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		Command cmd = null;
-		
+
 		while (keepRunning) {
-			
+
 			cmd = inQueue.poll();
-			
+
 			if (null == cmd) {
-				try {
-					inQueue.wait();
-				} catch (InterruptedException e) {	/* do nothing. */ }
-				
+				synchronized(inQueue) {
+					try {
+						inQueue.wait();
+					} catch (InterruptedException e) {	/* do nothing. */ }
+				}
+
 			} else {
 				cmd.execute();
 			}
@@ -34,7 +36,9 @@ public class HandlerThread extends Thread implements PostCommand {
 
 	@Override
 	public void post(Command cmd) {
-		inQueue.add(cmd);
-		inQueue.notifyAll();
+		synchronized(inQueue) {
+			inQueue.add(cmd);
+			inQueue.notifyAll();
+		}
 	}
 }
