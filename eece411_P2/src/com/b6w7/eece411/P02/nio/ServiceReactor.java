1eee4253b3f3e0296ad42344320255c8da3a06e2
Scott <scotthazlett@gmail.com>
2014-03-20 20:15:17 -0700

    Port number now attached to url.  Routing with 2 nodes working.

--- eece411_P2/src/com/b6w7/eece411/P02/nio/ServiceReactor.java
+++ eece411_P2/src/com/b6w7/eece411/P02/nio/ServiceReactor.java
@@ -124,12 +124,11 @@ public class ServiceReactor implements Runnable, JoinThread {
 				
 				SocketRegisterData data;
 				while (registrations.size() > 0) {
-					if(IS_SHORT) System.out.println("--- Found registration to connect to 11112");
+					if(!IS_SHORT) System.out.println("--- ServiceReactor()::run() connecting to remote host");
 					data = registrations.poll();
 					data.key = data.sc.register(selector, data.ops, data.cmd);
 
-					data.sc.connect(new InetSocketAddress(11112));
-					if(IS_SHORT) System.out.println("--- checkLocal() woke up selector");
+					data.sc.connect(data.addr);
 				}
 				
 			} catch (IOException ex) { /* ... */ }
